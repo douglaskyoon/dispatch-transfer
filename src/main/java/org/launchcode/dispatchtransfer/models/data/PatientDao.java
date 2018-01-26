a90db8bf73e7b157747d1f5b76b4015d100c22f8
Doug Yoon <douglaskyoon@gmail.com>
2018-01-25 18:28:34 -0600

    set up a database

--- src/main/java/org/launchcode/dispatchtransfer/models/data/PatientDao.java
+++ src/main/java/org/launchcode/dispatchtransfer/models/data/PatientDao.java
@@ -2,11 +2,14 @@ package org.launchcode.dispatchtransfer.models.data;
 
 
 import org.launchcode.dispatchtransfer.models.Patient;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 
 @Repository
-
+@Transactional
 public interface PatientDao extends CrudRepository<Patient, Integer> {
 
 
